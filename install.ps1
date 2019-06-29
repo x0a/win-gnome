d36dd0ed0f7539d305578ca6f00301b499c69bd7
x0a <x0a.cake@gmail.com>
2019-06-29 00:33:45 -0500

    check for process before attempting kill

--- install.ps1
+++ install.ps1
@@ -5,8 +5,12 @@ $targetfinal = ".\target\release\$target"
 $destination = "$Env:Programfiles\WinGnome"
 $destfinal = "$destination\$target"
 $taskname = "WinGnome"
-Write-Host "Killing any currently running instance of win-gnome.exe..."
-Stop-Process -Name $targetname -Force -Confirm:$false >$null
+
+Get-Process $targetname -OutVariable process_exists >$null
+if($process_exists){
+    Write-Host "Killing any currently running instance of win-gnome.exe..."
+    Stop-Process -Name $targetname -Force
+}
 Write-Host "Creating $destination"
 New-Item -ItemType Directory -Force -Path $destination >$null
 Write-Host "Placing $target in $destination"
@@ -15,13 +19,14 @@ $trigger = New-ScheduledTaskTrigger -AtLogOn # Specify the trigger settings
 $user = "$env:USERDOMAIN\$env:UserName" # Specify the account to run the script
 $action = New-ScheduledTaskAction -Execute $destfinal # Specify what program to run and with its parameters
 $description = "Start WinGnome at login"
-Get-ScheduledTask -TaskName $taskname -ErrorAction SilentlyContinue -OutVariable task_exists > $null
 
+Get-ScheduledTask -TaskName $taskname -ErrorAction SilentlyContinue -OutVariable task_exists > $null
 if ($task_exists) {
-    Write-Host "Removing any previous  '$taskname' scheduled task"
+    Write-Host "Removing previous  '$taskname' scheduled task"
     Unregister-ScheduledTask -TaskName $taskname -Confirm:$false
 }
+
 Write-Host "Registering new '$taskname' scheduled task"
-Register-ScheduledTask -TaskName $taskname -Trigger $trigger -User $User -Action $action -Description $description -RunLevel Highest -Force # Specify the name of the task
+Register-ScheduledTask -TaskName $taskname -Trigger $trigger -User $User -Action $action -Description $description -RunLevel Highest -Force >$null
 Write-Host "Starting '$taskname'"
 Start-ScheduledTask -TaskName $taskname
