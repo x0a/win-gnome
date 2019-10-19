bbedff033dd23731d757e6e1c33788abd19a5691
x0a <x0a.cake@gmail.com>
2019-10-19 04:05:17 -0500

    request elevated privileges if admin

--- uninstall.ps1
+++ uninstall.ps1
@@ -20,16 +20,11 @@ if(!$currentprincipal.IsInRole($admin)){
     # Exit from the current, unelevated, process
     exit
 }
-# Installation start
+# Uninstallation start
+
 $targetname = "win-gnome"
 $target = "$targetname.exe"
-if(Test-Path ($PSScriptRoot + ".\target\release\$target")){
-    $targetfinal = $PSScriptRoot + ".\target\release\$target"
-}else{
-    $targetfinal = $PSScriptRoot + ".\$target"
-}
 $destination = "$Env:Programfiles\WinGnome"
-$destfinal = "$destination\$target"
 $taskname = "WinGnome"
 
 Get-Process $targetname -Erroraction "silentlycontinue" -OutVariable process_exists >$null
@@ -37,25 +32,14 @@ if($process_exists){
     Write-Host "Killing any currently running instance of win-gnome.exe..."
     iex "taskkill /im win-gnome.exe" >$null
 }
-Write-Host "Creating $destination"
-New-Item -ItemType Directory -Force -Path $destination >$null
-Write-Host "Placing $target in $destination"
-Copy-Item -Path $targetfinal -Destination $destination -Force
-$trigger = New-ScheduledTaskTrigger -AtLogOn # Specify the trigger settings
-$user = "$env:USERDOMAIN\$env:UserName" # Specify the account to run the script
-$action = New-ScheduledTaskAction -Execute $destfinal # Specify what program to run and with its parameters
-$description = "Start WinGnome at login"
 
 Get-ScheduledTask -TaskName $taskname -ErrorAction SilentlyContinue -OutVariable task_exists > $null
 if ($task_exists) {
     Write-Host "Removing previous  '$taskname' scheduled task"
     Unregister-ScheduledTask -TaskName $taskname -Confirm:$false
 }
-
-Write-Host "Registering new '$taskname' scheduled task"
-Register-ScheduledTask -TaskName $taskname -Trigger $trigger -User $User -Action $action -Description $description -RunLevel Highest -Force >$null
-Write-Host "Starting '$taskname'"
-Start-ScheduledTask -TaskName $taskname
+Write-Host "Removing installed directories"
+Remove-Item -Recurse -Force $destination
 
 Write-Host "Press any key to exit..."
 [void][System.Console]::ReadKey($true)
